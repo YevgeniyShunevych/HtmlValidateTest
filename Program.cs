a15be564974b3fe8ba4c2dc49f4abdc99f355615
Aydin Erdas <info@aydinerdas.com>
2024-05-10 10:14:58 +0200

    Test case

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Atata.Cli.HtmlValidate;
+using Atata.HtmlValidation;
+
+namespace HtmlValidateTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var validator = new HtmlValidator(new HtmlValidationOptions
+        {
+            OutputFormatter = HtmlValidateFormatter.Names.Json,
+            ResultFileFormatter = HtmlValidateFormatter.Names.Json,
+            SaveResultToFile = true
+        });
+        // read the reddit-frontpage.html file into a string
+        string html = File.ReadAllText("reddit-frontpage.html");
+        
+        // These are just to show that the html file was read correctly
+        Console.WriteLine(html.Substring(0, 10));
+        Console.WriteLine(html.Substring(html.Length - 10));
+        
+        var validationResult = validator.Validate(html);
+        
+        // These are just to show that the validation result was cut off.
+        Console.WriteLine(validationResult.Output.Substring(0, 10));
+        Console.WriteLine(validationResult.Output.Substring(validationResult.Output.Length - 10));
+        
+        Console.WriteLine($"Length of output: {validationResult.Output.Length}");
+    }
+}
